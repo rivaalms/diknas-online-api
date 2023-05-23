d6cdd922f9aa17fce0a25bdee79991e38f0296d9
rivaalms <rvalms@icloud.com>
2023-05-23 19:47:52 +0700

    Refactor RevisionController's `getRevisionData` to use the request object
    Instead of using the `$id` parameter to query the revisions data, this commit changes the method signature to accept a request object, then uses `$request->id` instead. This refactoring simplifies the method and increases code reusability.

--- app/Http/Controllers/RevisionController.php
+++ app/Http/Controllers/RevisionController.php
@@ -2,13 +2,14 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Revision;
 use Carbon\Carbon;
+use App\Models\Revision;
+use Illuminate\Http\Request;
 
 class RevisionController extends Controller
 {
-   public function getRevisionData($id) {
-      $data = Revision::where('data_id', $id)->orderBy('updated_at', 'desc')->get();
+   public function getRevision(Request $request) {
+      $data = Revision::where('data_id', $request->id)->orderBy('updated_at', 'desc')->get();
       foreach ($data as $d) {
          $d->date = Carbon::parse($d->updated_at)->locale('id_ID')->translatedFormat('d F Y H:i');
          // dd($d->updated_at);
