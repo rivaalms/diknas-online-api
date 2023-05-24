123567c97acfeecd44b05e16c568e88ccfa890dd
rivaalms <rvalms@icloud.com>
2023-05-24 19:06:15 +0700

    Refactor routes web.php for middleware grouping and organization
    This commit refactors routes/web.php to group routes by middleware and simplify route organization. This makes it easier to manage route access based on user roles.

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,6 @@
 
 /** @var \Laravel\Lumen\Routing\Router $router */
 
-use App\Http\Controllers\SchoolController;
-
 /*
 |--------------------------------------------------------------------------
 | Application Routes
@@ -25,66 +23,74 @@ $router->post('/diknas/login', 'DiknasController@login');
 $router->post('/admin/login', 'UserController@login');
 
 $router->group(['middleware' => ['auth']], function() use($router) {
+    
+    $router->group(['middleware' => ['school']], function() use($router) {
+        $router->get('/school/getSelf', 'SchoolController@getSchoolLogin');
+        $router->post('/school/logout', 'SchoolController@logout');
+        $router->put('/school/updatepassword/{id}', 'SchoolController@updatePassword');
+        $router->put('/school/data/{id}/update', 'DataController@updateSchool');
+        $router->get('/getStudentTeacherCount', 'SchoolController@getStudentTeacherCount');
+        $router->get('/getStudents', 'SchoolStudentController@getStudents');
+        $router->get('/getStudentsYear', 'SchoolStudentController@getStudentsYear');
+        $router->post('/storeStudents', 'SchoolStudentController@storeStudents');
+        $router->get('/getTeachers', 'SchoolTeacherController@getTeachers');
+        $router->get('/getTeachersYear', 'SchoolTeacherController@getTeachersYear');
+        $router->post('/storeTeachers', 'SchoolTeacherController@storeTeachers');
+    });
+    
+    $router->group(['middleware' => ['supervisor']], function() use($router) {
+        $router->post('/supervisor/logout', 'SupervisorController@logout');
+        $router->get('/supervisor/getSelf', 'SupervisorController@getSelf');
+        $router->get('/supervisor/getStudentsYear/{id}', 'SchoolStudentController@getStudentsYearSupervisor');
+        $router->get('/supervisor/getSchoolBySupervisor/{id}', 'SupervisorController@getSchoolBySupervisor');
+        $router->put('/supervisor/updatepassword/{id}', 'SupervisorController@updatePassword');
+        $router->get('/supervisor/getData', 'DataController@getDataBySupervisor');
+        $router->post('/supervisor/verifyData', 'DataController@verifyData');
+        $router->post('/supervisor/revisionData', 'DataController@revisionData');
+    });
+    
+    $router->group(['middleware' => ['diknas']], function() use($router) {
+        $router->post('/diknas/logout', 'DiknasController@logout');
+        $router->get('/diknas/getSelf', 'DiknasController@getSelf');
+        $router->put('/diknas/updatepassword/{id}', 'DiknasController@updatePassword');
+    });
+
+    $router->group(['middleware' => ['admin']], function() use($router) {
+        $router->post('/admin/logout', 'UserController@logout');
+        $router->get('/admin', 'UserController@index');
+        $router->get('/admin/getSelf', 'UserController@getSelf');
+        $router->get('/admin/countUsers', 'UserController@countUsers');
+        $router->post('/admin/create', 'UserController@store');
+        $router->put('/admin/update/{id}', 'UserController@update');
+        $router->delete('/admin/delete/{id}', 'UserController@delete');
+        $router->put('/admin/updatepassword/{id}', 'UserController@updatepassword');
+        $router->put('/data/update/{id}', 'DataController@update');
+        $router->delete('/data/delete/{id}', 'DataController@delete');
+        $router->delete('/school/delete/{id}', 'SchoolController@destroy');
+        $router->delete('/supervisor/delete/{id}', 'SupervisorController@delete');
+        $router->delete('/diknas/delete/{id}', 'DiknasController@delete');
+    });
+
     $router->get('/school', 'SchoolController@index');
     $router->post('/school/create', 'SchoolController@store');
-    $router->get('/school/getSelf', 'SchoolController@getSchoolLogin');
     $router->get('/school/get/{id}', 'SchoolController@getSingle');
     $router->put('/school/update/{id}', 'SchoolController@update');
-    $router->delete('/school/delete/{id}', 'SchoolController@destroy');
-    $router->post('/school/logout', 'SchoolController@logout');
-    $router->put('/school/updatepassword/{id}', 'SchoolController@updatePassword');
-
-    $router->put('/school/data/{id}/update', 'DataController@updateSchool');
-    
     $router->get('/countSchool', 'SchoolController@countSchool');
-    $router->get('/getStudentTeacherCount', 'SchoolController@getStudentTeacherCount');
-    $router->get('/getStudents', 'SchoolStudentController@getStudents');
-    $router->get('/getStudentsYear', 'SchoolStudentController@getStudentsYear');
-    $router->post('/storeStudents', 'SchoolStudentController@storeStudents');
-    $router->get('/getTeachers', 'SchoolTeacherController@getTeachers');
-    $router->get('/getTeachersYear', 'SchoolTeacherController@getTeachersYear');
-    $router->post('/storeTeachers', 'SchoolTeacherController@storeTeachers');
     $router->get('/getRevision', 'RevisionController@getRevision');
     $router->get('/getAllSchool', 'SchoolController@getAllSchool');
 
     $router->get('/supervisor', 'SupervisorController@index');
     $router->get('/supervisor/getAll', 'SupervisorController@getAll');
-    $router->post('/supervisor/logout', 'SupervisorController@logout');
-    $router->get('/supervisor/getSelf', 'SupervisorController@getSelf');
-    $router->get('/supervisor/getSchoolBySupervisor/{id}', 'SupervisorController@getSchoolBySupervisor');
-    $router->get('/supervisor/getStudentsYear/{id}', 'SchoolStudentController@getStudentsYearSupervisor');
-    
-    $router->get('/supervisor/getData', 'DataController@getDataBySupervisor');
-    $router->post('/supervisor/verifyData', 'DataController@verifyData');
-    $router->post('/supervisor/revisionData', 'DataController@revisionData');
-
-    $router->put('/supervisor/updatepassword/{id}', 'SupervisorController@updatePassword');
     $router->post('/supervisor/create', 'SupervisorController@store');
     $router->put('/supervisor/update/{id}', 'SupervisorController@update');
-    $router->delete('/supervisor/delete/{id}', 'SupervisorController@delete');
     $router->get('/getSchoolSupervisorCount', 'DiknasController@getSchoolSupervisorCount');
 
-    $router->post('/diknas/logout', 'DiknasController@logout');
     $router->get('/diknas', 'DiknasController@index');
-    $router->get('/diknas/getSelf', 'DiknasController@getSelf');
-    // $router->get('/diknas/getData', 'DataController@getVerifiedData');
-    $router->get('/getSchoolStats', 'DiknasController@getSchoolStats');
-    $router->put('/diknas/updatepassword/{id}', 'DiknasController@updatePassword');
     $router->post('/diknas/create', 'DiknasController@store');
     $router->put('/diknas/update/{id}', 'DiknasController@update');
-    $router->delete('/diknas/delete/{id}', 'DiknasController@delete');
-
+    
+    $router->get('/getSchoolStats', 'DiknasController@getSchoolStats');
     $router->get('/getStudentTeacherYearList', 'DiknasController@getStudentTeacherYearList');
-    
-    $router->post('/admin/logout', 'UserController@logout');
-    $router->get('/admin', 'UserController@index');
-    $router->get('/admin/getSelf', 'UserController@getSelf');
-    $router->get('/admin/countUsers', 'UserController@countUsers');
-    $router->post('/admin/create', 'UserController@store');
-    $router->put('/admin/update/{id}', 'UserController@update');
-    $router->delete('/admin/delete/{id}', 'UserController@delete');
-    $router->put('/admin/updatepassword/{id}', 'UserController@updatepassword');
-    
     $router->get('/searchSchoolFilter', 'DataController@searchSchoolFilter');
     $router->get('/getCategories', 'CategoryController@index');
     $router->get('/getDataTypes', 'CategoryController@getDataTypes');
@@ -93,8 +99,6 @@ $router->group(['middleware' => ['auth']], function() use($router) {
     $router->get('/getData', 'DataController@index');
     $router->get('/getData/{id}', 'DataController@getDataById');
     $router->post('/data/create', 'DataController@create');
-    $router->put('/data/update/{id}', 'DataController@update');
-    $router->delete('/data/delete/{id}', 'DataController@delete');
     $router->get('/getSchoolType', 'SchoolController@getSchoolType');
     $router->get('/getDataYear', 'DataController@getDataYear');
 });
